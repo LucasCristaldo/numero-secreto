5043764545cf9f6cc72a92447fd8c859ae1c5ebd
Lucas Cristaldo <lucas.bob10@hotmail.com>
2025-07-10 06:50:11 -0300

    first commit

---
+++ js-curso-2-aula_1/app.js
@@ -0,0 +1,103 @@
+let numeroSorteados = [];// Aqui so guarda os valores que já foram sorteados, para que eles nao se repitam
+let numeroSorteadoMax = 10;
+let numero_aleatorio = gerarNumeroAleatorio(); 
+// Aqui dentro tem um return de um numero, e antes ele verifica se pode entrar na lista, se ele ja entrou uma vez,
+// vai olhar para o numeroSorteados e vai ver se esse cara já entrou aqui antes.
+
+let tentativas = 1;
+
+/*
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+*/
+
+function exibirTextoNaTela(tag, texto)
+{
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', 
+        {rate:1.3}
+    );
+}
+
+
+function gerarNumeroAleatorio()
+{
+    let numerozinho = parseInt(Math.random() * numeroSorteadoMax + 1);
+    // se eu botei 10, ele só pode tirar o valor 10, se eu botar o '+1'
+    let tamanhoDaLista = numeroSorteados.length;
+    if (tamanhoDaLista == numeroSorteadoMax)
+    {
+        numeroSorteados = [];
+    }
+
+    if (numeroSorteados.includes(numerozinho))
+    {
+        return gerarNumeroAleatorio();
+    } 
+    else
+    {
+        numeroSorteados.push(numerozinho);
+        return numerozinho;
+    }
+
+}
+
+function exibirMensagemInicial()
+{
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+
+function limparCampo()
+{
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function verificarNovoJogo()
+{
+    numero_aleatorio = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function verificarChute()
+{
+    let chute = document.querySelector('input').value
+   
+    if (numero_aleatorio == chute)
+    {
+        let trys = tentativas > 1 ? 'tentativas' : 'tentativa';
+        exibirTextoNaTela('h1', 'ACERTOU!');
+        exibirTextoNaTela('P', `Você acertou o número secreto com ${tentativas} ${trys} !`);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    }
+    else
+    {
+        limparCampo();
+        if (numero_aleatorio > chute)
+        {
+            exibirTextoNaTela('p', 'O número secreto é MAIOR');
+        }
+        else
+        {
+            exibirTextoNaTela('p', 'O número secreto é MENOR!');
+        }
+        tentativas++;
+    }
+
+    
+}
---
+++ js-curso-2-aula_1/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,35 @@
+function oimundo()
+{
+    alert("Olá Mundo");
+}
+
+function oinome(nome)
+{
+    console.log(`Olá ${nome}`)
+}
+
+oimundo();
+
+oinome('lulu');
+
+
+function memultiplique(numero)
+{
+    return numero * numero;
+}
+
+console.log(memultiplique(12));
+
+fatorial(5);
+
+function fatorial(numero)
+{
+    
+    for (let index = numero; index >= 1; index--) {
+        
+        numero *= index;
+        console.log(`${fanut}`);
+    }
+
+    alert(fat);
+}
